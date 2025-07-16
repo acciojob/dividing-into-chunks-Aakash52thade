54fe46f3f3d24f9b9f38829d6b03cb17d1c283f2
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-16 06:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+kafdsconst arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result = [];
+	let current = [];
+	let sum = 0;
+
+	for(let i=0; i<arr.length; i++){
+		let num = arr[i];
+
+		if(sum + num <= n){
+			current.push(num);
+			sum += num;
+		}else{
+			if(current.length > 0){
+				result.push(num);
+				sum = num;
+			}
+		}
+		if(current.length > 0){
+			result.push(current);
+		}
+	}
+	return result;
 };
 
-const n = prompt("Enter n: ");
+const n = prompt("Enter n: "); 
 alert(JSON.stringify(divide(arr, n)));
